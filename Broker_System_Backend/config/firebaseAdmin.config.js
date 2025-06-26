a2eb1381519c6b5bb34b750c9c1dce274f3a8622
HP <rediet754@gmail.com>
2025-06-26 00:12:42 -0700

    update

--- Broker_System_Backend/config/firebaseAdmin.config.js
+++ Broker_System_Backend/config/firebaseAdmin.config.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-var serviceAccount = require("../broker-system-2f8c5-firebase-adminsdk-j0z57-a6310e5fb1.json");
+var serviceAccount = require(process.env.FIREBASE_CONFIG);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
